afe4653efd1b6ffbf20735563343885371d7b465
Dipak8698 <122249728+Dipak8698@users.noreply.github.com>
2023-07-02 09:27:25 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app"
+    bucket = "dipakc"
     region = "us-east-1"
-    key = "eks/terraform.tfstate"
+    key = "terraform-jenkins/terraform.tfstate"
   }
 }
